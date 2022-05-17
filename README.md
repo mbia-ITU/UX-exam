b570e8c9fe88e08809f4df467824c2605081c1c0
Amalie (amdh) <amdh@itu.dk>
2022-05-17 16:30:39 +0200

    Set-Up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# UX_gruppe16_eksamen
+# User experience Og Webprogrammering - Assignment #03
+
+We have modified the original starting code for the [Friendly Chat app](https://codelabs.developers.google.com/codelabs/firebase-web/).
+
+You can extend this project to use a front-end framework as [Bootstrap](https://getbootstrap.com), [Semantic UI](https://semantic-ui.com), [Foundation](https://get.foundation), [Materialize](https://materialize.com), among to others.
+
+
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/firebase-messaging-sw.js":
+/*!**************************************!*\
+  !*** ./src/firebase-messaging-sw.js ***!
+  \**************************************/
+/***/ (() => {
+
+eval("// TODO: Set up Firebase Cloud Messaging service worker//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZmlyZWJhc2UtbWVzc2FnaW5nLXN3LmpzLmpzIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdWV3cDIwMjIvLi9zcmMvZmlyZWJhc2UtbWVzc2FnaW5nLXN3LmpzP2U1OTkiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gVE9ETzogU2V0IHVwIEZpcmViYXNlIENsb3VkIE1lc3NhZ2luZyBzZXJ2aWNlIHdvcmtlciJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/firebase-messaging-sw.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/firebase-messaging-sw.js"]();
+/******/ 	
+/******/ })()
+;

c1310a4fbae9cdede5a7a78ef9bd2746da929c8f
Amalie (amdh) <amdh@itu.dk>
2022-05-23 15:10:16 +0200

    Location virker næsten

--- src/index.js
+++ src/index.js
@@ -109,7 +109,31 @@ function addSizeToGoogleProfilePic(url) {
     return url;
 }
 
+function initMap(position) {
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
 
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1hbGllaG9sbSIsImEiOiJjbDNpb3NvNnIwMWszM2VvN3M3M21jZWtlIn0.JiznQK_c9HSqaW39rlZRiA', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiYW1hbGllaG9sbSIsImEiOiJjbDNpb3NvNnIwMWszM2VvN3M3M21jZWtlIn0.JiznQK_c9HSqaW39rlZRiA'
+    }).addTo(map);
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initMap);
+    } else {
+        document.getElementById('errorLabel').innerHTML = "Error"
+    }
+}
+
+window.onload = function() {
+    getLocation()
+}
 
 // Shortcuts to DOM Elements.
 var userPicElement = document.getElementById('user-pic')
